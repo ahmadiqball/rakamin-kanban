db81fc82ef6c1abcaa8baa2b1312c5bffa5b9900
ahmadiqball <ahmad.iqbal@incentro.com>
2024-04-21 23:11:41 +0800

    chore: setup unocss design systems

--- uno.config.ts
+++ uno.config.ts
@@ -3,7 +3,45 @@ import { defineConfig } from 'unocss';
 export default defineConfig({
   theme: {
     fontFamily: {
-      'nunito': 'Nunito Sans',
-    }
-  }
-})
+      nunito: 'Nunito Sans',
+    },
+    boxShadow: {
+      'modal': '0px 4px 8px 0px rgba(0, 0, 0, 0.1)',
+      'dropdown': '0px 4px 4px 0px rgba(0, 0, 0, 0.08)',
+    },
+    colors: {
+      dark: {
+        500: '#404040',
+        600: '#333333',
+        700: '#1E1F21',
+      },
+      grey: {
+        300: '#FAFAFA',
+        400: '#EDEDED',
+        500: '#E0E0E0',
+        700: '#9E9E9E',
+        800: '#757575',
+      },
+      blue: {
+        100: '#F7FEFF',
+        300: '#4DB5BC',
+        700: '#01959F',
+      },
+      green: {
+        100: '#F8FBF9',
+        300: '#B8DBCA',
+        700: '#43936C',
+      },
+      orange: {
+        100: '#FFFCF5',
+        300: '#FEEABC',
+        700: '#FA9810',
+      },
+      red: {
+        100: '#FFFAFA',
+        300: '#F5B1B7',
+        700: '#E11428',
+      }
+    },
+  },
+});
